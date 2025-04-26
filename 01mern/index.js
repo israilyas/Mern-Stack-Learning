2c8c1292040190b0f4896d8603207859d3fb97de
ISRA ILYAS <israqureshi138@gmail.com>
2025-04-26 22:36:23 +0500

    started learning mern stack

---
+++ 01mern/index.js
@@ -0,0 +1,12 @@
+// Fundamentals of javaScript
+// Arrays and objects
+//Foreach, map, filter, find, indexOf
+
+var arr = [1,2,3,4]
+
+// For Each
+arr.forEach(function(val){
+console.log(val+" hello")
+})
+
+//Map
